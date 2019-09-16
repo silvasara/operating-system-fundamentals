0130d9f8c96d175529d121677517d5bc860a7caa
Sara Silva <saracsas2@gmail.com>
2019-09-16 05:23:45 -0300

    Add communication between processes

--- activity1.c
+++ activity1.c
@@ -3,93 +3,84 @@
 #include <sys/types.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 char S[101];
 
 void show_upper_case_letters(){
+    printf("Sem letras minúsculas: \n");
+
     for(int i = 0; i < strlen(S); i++){
         if(isupper(S[i]))
             printf("%c", S[i]);
     }
 
     printf("\n");
+    exit(1);
 }
 
 void show_lower_case_letters(){
+    printf("Sem letras maiúsculas: \n");
+
     for(int i = 0; i < strlen(S); i++){
         if(islower(S[i]))
             printf("%c", S[i]);
     }
 
     printf("\n");
+    exit(1);
 }
 
 void show_consonants(){
+    printf("Sem vogais: \n");
+
     for(int i = 0; i < strlen(S); i++){
         if(S[i] != 'a' && S[i] != 'e' && S[i] != 'i' && S[i] != 'o' && S[i] != 'u' &&
-           S[i] != 'A' &&  S[i] != 'E' && S[i] != 'I' && S[i] != 'O' && S[i] != 'U')
+                S[i] != 'A' &&  S[i] != 'E' && S[i] != 'I' && S[i] != 'O' && S[i] != 'U')
             printf("%c", S[i]);
     }
 
     printf("\n");
+    exit(1);
 }
 
 int main(){
+    signal(SIGUSR2, show_lower_case_letters);
+    signal(SIGUSR1, show_consonants);
+    signal(SIGTERM, show_upper_case_letters);
 
-    scanf("%s", S);
+    int status;
 
-    pid_t pid = fork();
+    scanf(" %s", S);
 
-    if(pid > 0){
-        pid_t pid3 = fork();
+    pid_t pid1 = fork(); 
 
-        if(pid3 > 0){ // parent 1001
-            printf("(1001)I'm the %d and I'm parent of (1003) %d\n", getpid(),  pid3); 
-        }
-        else{ // child 1003
-            printf("(1003)I'm the child: %d and my parent is (1001) %d\n", getpid(), getppid());
+    if(pid1 == 0){ // child 1002
+        pid_t pid4 = fork(); 
 
-            pid_t pid5 = fork();
+        if(pid4 == 0) // child 1004
+            kill(getpid(), SIGUSR2);
 
-            if(pid5 > 0){ // parent 1003
-                printf("(1003)I'm the %d and I'm parent of (1005) %d\n", getpid(),  pid5); 
-            }
-            else{ // child 1005
-                printf("(1005)I'm the child: %d and my parent is (1003) %d\n", getpid(), getppid());
-                printf("Tamanho da string S: %d\n", strlen(S));
-            }
-        }
-    }
-    else{ // child 1002
-        pid_t pid2 = fork();
+        wait(&status);
 
-        if(pid2 > 0){ // parent 1002 
-            printf("(1002)I'm the child: %d and my parent is (1001)%d\n", getpid(), getppid());
+        pid_t pid7 = fork();
 
-            pid_t pid6 = fork();
+        if(pid7 == 0) // child 1007
+            kill(getpid(), SIGUSR1);
 
-            if(pid6> 0){ // parent 1002
-                printf("(1002)I'm the %d and I'm parent of (1006)%d\n", getpid(),  pid6); 
+        wait(&status);
 
-                pid_t pid7 = fork();
+        pid_t pid6 = fork(); // child 1006
 
-                if(pid7 > 0){ // parent 1002
-                    printf("(1002)I'm the %d and I'm parent of (1007)%d\n", getpid(),  pid7); 
-                }
-                else{ // child 1007
-                    printf("(1007)I'm the child: %d and my parent is (1002) %d\n", getpid(), getppid());
-                    show_consonants();
-                }
+        if(pid6 == 0)
+            kill(getpid(), SIGTERM);
+    } 
+    else {
+        pid_t pid3 = fork(); // create - 1003
 
-            }
-            else{ // child 1006
-                printf("(1006)I'm the child: %d and my parent is (1002)%d\n", getpid(), getppid());
-                show_lower_case_letters();
-            }
-        }
-        else{ // child 1004
-            printf("(1004)I'm the child: %d and my parent is (1002)%d\n", getpid(), getppid());
-            show_upper_case_letters();
+        if(pid3 == 0){ // child 1005
+            printf("Tamanho da string S: %ld\n", strlen(S));
         }
     }
 
